b87b7ebd584b2302994c69d5ff3687443f750470
Luca Venturelli <luca@skyscrapers.eu>
2018-11-21 10:30:43 +0100

    fixes documentation and cloudtrail s3 policy This PR aligns the module to our standards It also cleans up the s3 bucket policy to the new AWS standards

--- cloudtrail-s3/main.tf
+++ cloudtrail-s3/main.tf
@@ -1,6 +1,10 @@
+locals {
+  bucket_name = "cloudtrail-${var.project}-${var.environment}"
+}
+
 module "cloudtrail" {
   source                        = "../cloudtrail"
-  bucket_name                   = "${var.bucket_name}"
+  bucket_name                   = "${local.bucket_name}"
   project                       = "${var.project}"
   environment                   = "${var.environment}"
   include_global_service_events = "${var.include_global_service_events}"
@@ -8,9 +12,8 @@ module "cloudtrail" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.bucket_name}"
+  bucket = "${local.bucket_name}"
   acl    = "private"
-
   versioning {
     enabled = "${var.versioning_enabled}"
   }
@@ -26,111 +29,66 @@ resource "aws_s3_bucket" "bucket" {
   }
 
   tags {
-    Name = "${var.bucket_name}"
+    Name = "${local.bucket_name}"
   }
 
-  policy = <<POLICY
-{
-	"Version": "2012-10-17",
-	"Id": "PutObjPolicy",
-	"Statement": [
-		{
-			"Sid": "DenyIncorrectEncryptionHeader",
-			"Effect": "Deny",
-			"Principal": "*",
-			"Action": "s3:PutObject",
-			"Resource": "arn:aws:s3:::${var.bucket_name}/*",
-			"Condition": {
-				"StringNotEquals": {
-					"s3:x-amz-server-side-encryption": "AES256"
-				}
-			}
-		},
-		{
-			"Sid": "DenyUnEncryptedObjectUploads",
-			"Effect": "Deny",
-			"Principal": "*",
-			"Action": "s3:PutObject",
-			"Resource": "arn:aws:s3:::${var.bucket_name}/*",
-			"Condition": {
-				"Null": {
-					"s3:x-amz-server-side-encryption": "true"
-				}
-			}
-		},
-		{
-			"Sid": "AWSCloudTrailAclCheck20131101",
-			"Effect": "Allow",
-			"Principal": {
-				"AWS": [
-					"arn:aws:iam::113285607260:root",
-					"arn:aws:iam::859597730677:root",
-					"arn:aws:iam::977081816279:root",
-					"arn:aws:iam::216624486486:root",
-					"arn:aws:iam::086441151436:root",
-					"arn:aws:iam::388731089494:root",
-					"arn:aws:iam::035351147821:root",
-					"arn:aws:iam::492519147666:root",
-					"arn:aws:iam::284668455005:root",
-					"arn:aws:iam::814480443879:root",
-					"arn:aws:iam::903692715234:root"
-				]
-			},
-			"Action": "s3:GetBucketAcl",
-			"Resource": "arn:aws:s3:::${var.bucket_name}"
-		},
-		{
-			"Sid": "AWSCloudTrailWrite20131101",
-			"Effect": "Allow",
-			"Principal": {
-				"AWS": [
-					"arn:aws:iam::113285607260:root",
-					"arn:aws:iam::859597730677:root",
-					"arn:aws:iam::977081816279:root",
-					"arn:aws:iam::216624486486:root",
-					"arn:aws:iam::086441151436:root",
-					"arn:aws:iam::388731089494:root",
-					"arn:aws:iam::035351147821:root",
-					"arn:aws:iam::492519147666:root",
-					"arn:aws:iam::284668455005:root",
-					"arn:aws:iam::814480443879:root",
-					"arn:aws:iam::903692715234:root"
-				]
-			},
-			"Action": "s3:PutObject",
-			"Resource": [
-        ${var.allowed_bucket_dist}
-			],
-			"Condition": {
-				"StringEquals": {
-					"s3:x-amz-acl": "bucket-owner-full-control"
-				}
-			}
-		},
-		{
-			"Sid": "AWSCloudTrailAclCheck20150319",
-			"Effect": "Allow",
-			"Principal": {
-				"Service": "cloudtrail.amazonaws.com"
-			},
-			"Action": "s3:GetBucketAcl",
-			"Resource": "arn:aws:s3:::${var.bucket_name}"
-		},
-		{
-			"Sid": "AWSCloudTrailWrite20150319",
-			"Effect": "Allow",
-			"Principal": {
-				"Service": "cloudtrail.amazonaws.com"
-			},
-			"Action": "s3:PutObject",
-			"Resource": "arn:aws:s3:::${var.bucket_name}/*",
-			"Condition": {
-				"StringEquals": {
-					"s3:x-amz-acl": "bucket-owner-full-control"
-				}
-			}
-		}
-	]
+  policy = "${data.aws_iam_policy_document.s3_permissions.json}"
 }
-POLICY
+
+data "aws_iam_policy_document" "s3_permissions" {
+  statement {
+    effect  = "Deny"
+    actions = ["s3:PutObject"]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}/*",
+    ]
+    principals {
+       type        = "*"
+       identifiers = ["*"]
+     }
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+
+      values = [
+        "AES256",
+      ]
+    }
+  }
+
+  statement {
+    actions = ["s3:GetBucketAcl"]
+
+    principals = {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}",
+    ]
+  }
+
+  statement {
+    actions = ["s3:PutObject"]
+
+    principals = {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control",
+      ]
+    }
+  }
 }
